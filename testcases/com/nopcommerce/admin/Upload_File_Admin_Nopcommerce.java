8f6ef4993d3a0791797f4e7da02f3515295270ae
Dungdepzai <tuandunghust94@gmail.com>
2022-06-01 03:55:40 -0700

    update log4j

--- testcases/com/nopcommerce/admin/Upload_File_Admin_Nopcommerce.java
+++ testcases/com/nopcommerce/admin/Upload_File_Admin_Nopcommerce.java
@@ -4,25 +4,24 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import commons.BaseTestNopcommerce;
-import commons.MethodListener;
 import commons.PageObjectGeneratorAdminNopcommerce;
 import pageObject.admin.nopCommerce.LoginPageObject;
 import pageObject.admin.nopCommerce.MyDasboardPageObject;
 import pageObject.admin.nopCommerce.ProductDetailPageObject;
 import pageObject.admin.nopCommerce.ProductSearchPageObject;
+
 //@Listeners(MethodListener.class)
 public class Upload_File_Admin_Nopcommerce extends BaseTestNopcommerce {
 	WebDriver driver;
 	String fileName, disPlayOrder, alt, title, clickToOrderIconTimes;
-	
+
 	@Parameters({ "browser", "appUrl" })
 	@BeforeClass
-	
+
 	public void initBrowser(String browserName, String appUrl) {
 		driver = getBrowserDriver(browserName, appUrl);
 		fileName = "Lion.jpg";
@@ -46,29 +45,31 @@ public class Upload_File_Admin_Nopcommerce extends BaseTestNopcommerce {
 
 	@Test
 	public void Admin_01_Upload_File() {
-
+		log.info("Step 1 - Test");
 		productDetailPage.clickToExpandPanelByName("far fa-images");
+		log.info("Step 1 - Test");
 		productDetailPage.uploadPictureByFileIndex("1", fileName);
-		verifyFalse(productDetailPage.isPictureUploadedSuccessByFileName(fileName));
+		log.info("Step 1 - Test");
+		verifyTrue(productDetailPage.isPictureUploadedSuccessByFileName(fileName));
 		productDetailPage.endterToAltTextbox(alt);
 		productDetailPage.endterToTitleTextbox(title);
 		productDetailPage.clickToOrderIcon("up", clickToOrderIconTimes);
 		productDetailPage.clickToAddProductButton();
 		productDetailPage.sleepInSecond(5);
-		verifyFalse(productDetailPage.isPictureDisplayed(clickToOrderIconTimes, alt, title));
+		verifyTrue(productDetailPage.isPictureDisplayed(clickToOrderIconTimes, alt, title));
 		productSearchPage = productDetailPage.clickToSaveButton();
-		verifyFalse(productSearchPage.isSuccessUploadFileTextDisplay());
+		verifyTrue(productSearchPage.isSuccessUploadFileTextDisplay());
 		productSearchPage.enterToTextboxByName("Adobe Photoshop CS4", "SearchProductName");
 		productSearchPage.clickToSearchButton();
-		verifyFalse(productSearchPage.isPictureUploaded("adobe-photoshop-cs4", "Adobe Photoshop CS4"));
+		verifyTrue(productSearchPage.isPictureUploaded("adobe-photoshop-cs4", "Adobe Photoshop CS4"));
 		productDetailPage = productSearchPage.clickToEditButton();
 		productDetailPage.clickToExpandPanelByName("far fa-images");
 		productDetailPage.clickToDeleteButtonAtPictureName();
-		Assert.assertTrue(productDetailPage.isDeletedMessageDisplayed());
+		verifyTrue(productDetailPage.isDeletedMessageDisplayed());
 		productSearchPage = productDetailPage.clickToSaveButton();
 		productSearchPage.enterToTextboxByName("Adobe Photoshop CS4", "SearchProductName");
 		productSearchPage.clickToSearchButton();
-		Assert.assertTrue(productSearchPage.isPictureUploaded("default-image", "Adobe Photoshop CS4"));
+		verifyTrue(productSearchPage.isPictureUploaded("default-image", "Adobe Photoshop CS4"));
 	}
 
 	@Test
